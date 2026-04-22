36fe401f095b4d28065c55cdb391a17d6defdc3c
kernelk14 <khyleisaias@gmail.com>
2026-04-22 18:20:15 +0800

    Revert "I made a mistake"
    This reverts commit 3bb35a7f1db721eb761c59a0fa83716abb1ce540.

--- index.php
+++ index.php
@@ -12,17 +12,37 @@
         echo '<script>console.log("Database connected successfully.")</script>';
     }
 
-    $query = "SELECT COUNT(*) AS total FROM Students";
+    $query = "SELECT * FROM Students";
     $result = $conn->query($query);
-    $row = $result->fetch_assoc();
-    $count = (int)$row;
+    $count = (int)$result->num_rows;
 
-    function printCard($row) {
-        echo "<div class='container border border-secondary rounded-3 mt-3'>";
-        echo "<h3><b>" . $row['last_name'] . ", " . $row['first_name'] . " " . $row['mid_name'] . "</b></h3>";
-        echo "<h4>" . $row['student_number'] . " | " . $row['email'] . "</h4>";
-        echo "<h4>" . $row['course'] . " " . $row['yr_lvl'] . "-" . $row['section'] . "</h4>";
-        echo "</div>";
+    function printCard($row): void {
+        $name = htmlspecialchars($row['last_name'] . ", " . $row['first_name'] . " " . $row['mid_name']);
+        $details = htmlspecialchars($row['student_number'] . " | " . $row['email']);
+        $course = htmlspecialchars($row['course'] . " " . $row['yr_lvl'] . "-" . $row['section']);
+        
+        echo "
+            <div class='container border border-secondary rounded-3 mt-3'>
+                <h3><b>{$name}</b></h3>
+                <h4>{$details}</h4>
+                <h4>{$course}</h4>
+            </div>";
+    }
+
+    function printRowCol($row): void {
+        $name = htmlspecialchars($row['last_name'] . ", " . $row['first_name'] . " " . $row['mid_name']);
+        $sid = htmlspecialchars($row['student_number']);
+        $email = htmlspecialchars($row['email']);
+        $course = htmlspecialchars($row['course'] . " " . $row['yr_lvl'] . "-" . $row['section']);
+
+        echo "
+            <tr>
+                <th scope='row'>{$sid}</th>
+                <td>{$name}</td>
+                <td>{$course}</td>
+                <td>{$email}</td>
+            </tr>
+        ";
     }
 
 ?>
@@ -31,7 +51,8 @@
 
 <html>
     <head>
-        <link rel="stylesheet" href="bootstrap/css/bootstrap.css" />
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" />
+        <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
         <title>Ports</title>
         <script>
             if (window.history.replaceState) {
@@ -40,19 +61,41 @@
         </script>
     </head>
     <body>
-        <form method="POST" action="actions/add_student">
+        <!--form method="POST" action="actions/add_student">
             <input type="submit" name="submit" value="Add Student" class="btn btn-outline-dark m-2 mt-2">
-        </form>
-        <h3 class="m-2">List of Students:</h3> 
-        <?php
-            if ($count < 1) echo "<h4>No records found.</h4>";
-            else {
-                $query = "SELECT * FROM Students ORDER BY last_name ASC";
-                $result = $conn->query($query);
-                while ($row = $result->fetch_assoc()) {
-                    printCard($row);
-                }
-            }
-        ?>
+        </form -->
+        <h3 class="m-2">List of Students:</h3>
+        <div class="container-fluid">
+            <table class="table table-striped table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th>Student Number</th>
+                        <th>Name</th>
+                        <th>Course, Year and Section</th>
+                        <th>Email Address</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        if ($count < 1) echo "<h4>No records found.</h4>";
+                        else {
+                            $query = "SELECT * FROM Students ORDER BY last_name ASC";
+                            $result = $conn->query($query);
+                            while ($row = $result->fetch_assoc()) {
+                                // printCard($row);
+                                printRowCol($row);
+                            }
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+        <div class="container">
+            <center>
+                <form method="POST" action="actions/add_student">
+                    <input type="submit" name="submit" value="Add Student" class="btn btn-outline-dark m-2 mt-2">
+                </form>
+            </center>
+        </div>
     </body>
 </html>
